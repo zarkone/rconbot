1412e05e4d8f5d3a0c12b58c51e38e5830292ff8
Anatoly Smolyaninov <zarkone@ya.ru>
2019-12-17 21:32:38 +0100

    move command write to func

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
-
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
-	"bufio"
 	"strings"
-	"context"
 )
+
+func sendRconCommand(conn net.Conn, command string, password string) (err error) {
+	udpMarker := []byte{0xFF, 0xFF, 0xFF, 0xFF}
+
+	log.Print("sending command...")
+	msg := udpMarker
+	cmdStr := fmt.Sprintf("rcon %s %s\n", password, command)
+
+	msg = append(msg, []byte(cmdStr)...)
+	bytes, err := conn.Write(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("bytes sent %d\n", bytes)
+
+	return nil
+}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
+
 	defer cancel()
 
 	password := "freeforall"
@@ -25,22 +43,13 @@ func main() {
 	defer conn.Close()
 	fmt.Println("connection created")
 
-	fmt.Println("sending command...")
-	msg := []byte{0xFF, 0xFF, 0xFF, 0xFF}
-	cmdStr := fmt.Sprintf("rcon %s %s\n", password, command)
-	msg = append(msg, []byte(cmdStr)...)
-	bytes, err := conn.Write(msg)
+	err = sendRconCommand(conn, command, password)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// bytes, err = fmt.Fprintf(conn, "rcon %s %s\n", password, command)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("bytes sent %d\n", bytes)
 
 	s := bufio.NewScanner(conn)
+
 	b := strings.Builder{}
 
 	for s.Scan() {
